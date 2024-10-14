610973f5f09de73324e86426373d117f2f48966d
jon <jkwei.86@gmail.com>
2024-10-13 21:14:35 -0400

    feat: allows game to be played utilizing the DOM

--- script.js
+++ script.js
@@ -1,36 +1,69 @@
+let humanChoice = "";
+let computerChoice = "";
+let humanScore = 0;
+let computerScore = 0;
+let round = 1;
 
-playGame();
+let humanScoreEle = document.querySelector('.humanScoreEle');
+let computerScoreEle = document.querySelector('.computerScoreEle');
+let playerChoice = document.querySelector('.playerChoice');
+let roundNum = document.querySelector('.roundNum');
+let msg = document.querySelector('.msgEle');
+let msgContainer = document.querySelector('.msgContainer');
+let playRoundBtn = document.querySelector('#playRoundBtn');
 
-function playGame(){
-    let humanSelection = getHumanChoice();
-    let computerSelection = getComputerChoice();
-    let humanScore = 0
-    let computerScore = 0
-    let round = 1
+[...document.querySelectorAll('.btn')].forEach(function (btn) {
+    btn.addEventListener('click',function(e){
+        humanChoice = e.target.id;
+        playerChoice.textContent = `${humanChoice}`;
+        return humanChoice
+    })
+})
+
+let playRound_btn = document.querySelector('#playRoundBtn');
     
-    function playRound(humanChoice, computerChoice){
-    humanChoice = humanChoice.toLowerCase();
+    playRound_btn.addEventListener('click', function(){
+        getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    })
+
+function getComputerChoice() {
+    computerChoice = Math.floor((Math.random() *3) +1)
+
+    switch (computerChoice){
+        case 1:
+            return computerChoice = "rock"
+        case 2:
+            return computerChoice = "scissors"
+        default:    
+            return computerChoice = "paper"
+    }
+}
+
+function playRound(humanChoice, computerChoice){
+    if (round > 5) {
+        let newMsg = document.createElement("p");
+        newMsg.textContent = "The game's finished! Would you like to play again? If so, click the 'Restart' button above!";
+        msgContainer.appendChild(newMsg);
+        playRoundBtn.remove();
+        return
+    }
     if(humanChoice === computerChoice){
-        console.group(`Round: ${round}`);
-        console.log(`You chose: ${humanChoice}`)
-        console.log(`Computer chose: ${computerChoice}`)
-        console.log("It's a tie! Nobody wins!");
-        console.log(`Your Score: ${humanScore} Computer Score: ${computerScore}`)
-        console.groupEnd();
-        round ++
+        roundNum.textContent = round
+        let newMsg = document.createElement("p");
+        newMsg.textContent = `Round ${round}: It's a tie! Nobody wins! You chose: ${humanChoice} the Computer chose: ${computerChoice}.`;
+        msgContainer.appendChild(newMsg)
     } else if (
         (humanChoice === "paper" && computerChoice === "scissors")||
         (humanChoice === "rock" && computerChoice === "paper")||
         (humanChoice === "scissors" && computerChoice === "rock")
             ){
                 computerScore ++
-                console.group(`Round: ${round}`);
-                console.log(`You chose: ${humanChoice}`)
-                console.log(`Computer chose: ${computerChoice}`)
-                console.log(`You lose! ${computerChoice} beats ${humanChoice}. Try again...`)
-                console.log(`Your Score: ${humanScore} Computer Score: ${computerScore}`)
-                console.groupEnd()
-                round ++
+                roundNum.textContent = round
+                computerScoreEle.textContent = computerScore
+                let newMsg = document.createElement("p");
+                newMsg.textContent= `Round ${round}: You lose! You chose: ${humanChoice} Computer chose: ${computerChoice} ${computerChoice} beats ${humanChoice}. Try again...`
+                msgContainer.appendChild(newMsg)
             }
     else if (
         (humanChoice === "paper" && computerChoice === "rock")||
@@ -38,46 +71,33 @@ function playGame(){
         (humanChoice === "scissors" && computerChoice === "paper")
             ){
                 humanScore ++
-                console.group(`Round: ${round}`);
-                console.log(`You chose: ${humanChoice}`)
-                console.log(`Computer chose: ${computerChoice}`)
-                console.log(`You win! ${humanChoice} beats ${computerChoice}. Nice job!`)
-                console.log(`Your Score: ${humanScore} Computer Score: ${computerScore}`)
-                console.groupEnd()
-                round ++
+                roundNum.textContent = round
+                humanScoreEle.textContent = humanScore
+                let newMsg = document.createElement("p");
+                newMsg.textContent = `Round ${round}: You win! You chose: ${humanChoice} Computer chose: ${computerChoice} ${humanChoice} beats ${computerChoice}. Nice job!`
+                msgContainer.appendChild(newMsg)
             }
-    }
-    playRound(humanSelection, computerSelection);
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
+            if (round < 5) round ++
+            else {round ++; checkWinner();}
 }
 
-function getHumanChoice(){
-    let humanChoice = prompt("What will you choose? (choices: Rock, Paper, Scissors)");
+function checkWinner() {
+    const restart = document.createElement('button');
+        restart.addEventListener('click', function(){
+            location.reload();
+        });
+        restart.textContent = "Restart";
 
-    return humanChoice;
-}
-
-function getComputerChoice() {
-    let computerChoice = Math.floor((Math.random() *3) +1)
-    
-    if (computerChoice === 1){
-        return computerChoice = "rock"
+    if (computerScore > humanScore) {
+        let newMsg = document.createElement("p");
+        newMsg.textContent = `You lose! The computer won ${computerScore} to ${humanScore}!`;
+        msgContainer.appendChild(newMsg);
+        return msgContainer.appendChild(restart);
     }
-    else if (computerChoice === 2){
-        return computerChoice = "scissors"
-    }
-    else{
-        return computerChoice = "paper"
+    else {
+        let newMsg = document.createElement("p");
+        newMsg.textContent = `You won! Nice job! You won ${humanScore} to ${computerScore}`;
+        msgContainer.appendChild(newMsg);
+        return msgContainer.appendChild(restart);
     }
 }
